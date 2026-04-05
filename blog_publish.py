41564179b31243134c66741c191b21da51774754
kulevi777-123 <kulevi777@gmail.com>
2026-04-05 09:22:40 -0700

    Update blog_publish.py

--- blog_publish.py
+++ blog_publish.py
@@ -1,46 +1,42 @@
 import os
 
-
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 import streamlit as st
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.chat_models import ChatOllama
+from langchain_core.globals import set_debug
 
-#from langchain_core.globals import set_debug
+set_debug(True)
 
-#set_debug(True)
-
- 
 OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+#OPENAI_API_KEY=""
 llm=ChatOpenAI(model="gpt-4o-mini",api_key=OPENAI_API_KEY)
 title_prompt= PromptTemplate(
     input_variables=["topic"],
     template ="""You are an experienced speech writer.
-    You need to write a blog post for
-    the following topic: {topic} 
+    You need to craft an impactful title for a speech
+    on the following topic: {topic} 
     Answer exactly with one title.
     """
     )
 
 speech_prompt= PromptTemplate(
-    input_variables=["topic"],
-    template ="""You need to write a blog post with Introduction , 3 main points with subpoints and conclusion
-    for the following topic: {topic} 
-
+    input_variables=["ABC","language"],
+    template ="""You need to write a powerful speech of 350 words
+    for the following title: {ABC}
+    Answer exactly with one title.
     """
     )
-EMOTIONS = [
-    "Inspiring", "Passionate", "Empathetic", "Motivational", "Heartfelt",
-    "Uplifting", "Empowering", "Sincere", "Hopeful", "Enthusiastic",
-]
-emotion = st.selectbox("Tone", EMOTIONS)
+first_chain = title_prompt | llm | StrOutputParser() | (lambda title: (st.write(title),title)[1])
+
+second_chain = speech_prompt | llm
+
+final_chain = first_chain | second_chain
 
 st.title("Speech Generator")
 topic = st.text_input("Enter a topic ")
+language = st.text_input("Enter a language ")
 
 if topic:
-    chain = speech_prompt | llm
-    response = chain.invoke({"topic": topic}, {"emotion": emotion})
+    response = final_chain.invoke({"topic":topic})
     st.write(response.content)
-
